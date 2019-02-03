fd3a7b6732ea8d16a40e456ce57fccd560e96a77
kyuriiii <kyuri99@naver.com>
2019-02-03 13:30:37 +0900

    winform study

---
+++ KyuriProject/KyuriProject/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KyuriProject
+{
+   
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+            
+            this.BackColor = Color.FromArgb(255, 232, 232);
+            this.Text = string.Empty; 
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+       
+    }
+}
